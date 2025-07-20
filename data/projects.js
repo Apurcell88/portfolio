e23d4f1a99d0d8c898837bd528b941d156918ab0
Adam Purcell <apurcell88@gmail.com>
2025-07-20 12:03:49 -0500

    Re-arranged data.

--- data/projects.js
+++ data/projects.js
@@ -1,18 +1,18 @@
 export const projects = [
   {
-    title: "Anonymous Clubhouse",
-    description: "A private blog board with user authentication.",
-    image: "/images/projects/anonymous.png",
-    tags: ["Node.js", "Express", "PostgreSQL", "Passport.js"],
-    link: "https://anonymous-clubhouse.onrender.com",
-    github: "https://github.com/Apurcell88/anonymous-clubhouse",
-  },
-  {
     title: "File Nest",
     description: "Securely manage your files.",
     image: "/images/projects/file-nest.png",
     tags: ["NextJS", "Prisma", "Cloudinary"],
     link: "https://file-nest-kappa.vercel.app/",
     github: "https://github.com/Apurcell88/FileNest",
+  },
+  {
+    title: "Anonymous Clubhouse",
+    description: "A private blog board with user authentication.",
+    image: "/images/projects/anonymous.png",
+    tags: ["Node.js", "Express", "PostgreSQL", "Passport.js"],
+    link: "https://anonymous-clubhouse.onrender.com",
+    github: "https://github.com/Apurcell88/anonymous-clubhouse",
   },
 ];
