99c5d279e5d9338bd0d0cdcffcca5ebd3d1ab528
Adam Purcell <apurcell88@gmail.com>
2025-07-26 16:18:49 -0500

    Added personal training to projects.

--- data/allProjects.js
+++ data/allProjects.js
@@ -8,6 +8,15 @@ export const allProjects = [
     github: "https://github.com/Apurcell88/anonymous-clubhouse",
   },
   {
+    title: "AP Fit Personal Training",
+    description:
+      "A sleek, responsive website to showcase my personal training services, fitness expertise, and client success stories.",
+    image: "/images/projects/personal-training.png",
+    tags: ["Node.js", "Next.js", "Prisma", "Tailwind"],
+    link: "https://personal-trainer-blush.vercel.app/",
+    github: "https://github.com/Apurcell88/personal-trainer",
+  },
+  {
     title: "Game Inventory App",
     description:
       "Manage games, genres, and developers with a PostgreSQL backend.",
--- data/projects.js
+++ data/projects.js
@@ -1,18 +1,19 @@
 export const projects = [
   {
+    title: "AP Fit Personal Training",
+    description:
+      "A sleek, responsive website to showcase my personal training services, fitness expertise, and client success stories.",
+    image: "/images/projects/personal-training.png",
+    tags: ["Node.js", "Next.js", "Prisma", "Tailwind"],
+    link: "https://personal-trainer-blush.vercel.app/",
+    github: "https://github.com/Apurcell88/personal-trainer",
+  },
+  {
     title: "File Nest",
     description: "Securely manage your files.",
     image: "/images/projects/file-nest.png",
     tags: ["NextJS", "Prisma", "Cloudinary"],
     link: "https://file-nest-kappa.vercel.app/",
     github: "https://github.com/Apurcell88/FileNest",
-  },
-  {
-    title: "Anonymous Clubhouse",
-    description: "A private blog board with user authentication.",
-    image: "/images/projects/anonymous.png",
-    tags: ["Node.js", "Express", "PostgreSQL", "Passport.js"],
-    link: "https://anonymous-clubhouse.onrender.com",
-    github: "https://github.com/Apurcell88/anonymous-clubhouse",
   },
 ];
